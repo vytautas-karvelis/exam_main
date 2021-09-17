2a56ff524198f584634a882e62412612b79b6295
vytautas-karvelis <vytkarvelis@gmail.com>
2021-09-17 09:50:00 +0200

    routes added

--- 2_backend/app.js
+++ 2_backend/app.js
@@ -1,7 +1,7 @@
 // Requiring packages
 const express = require('express');
 const cors = require('cors');
-const Car = require('./models/user');
+const User = require('./models/User');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -29,3 +29,49 @@ app.use(
   })
 );
 app.use(express.json());
+
+
+app.get("/api/users", (req, res)=>{
+    User.find()
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
+})
+
+app.post("/api/users", (req, res)=>{
+    if (!req.body.name || !req.body.age || !req.body.password || !req.body.email) {
+        res.status(400).json({ message: 'All fields are required' });
+        return;
+      }
+      // -- if valdiation passes, adding new car
+      const user = new User(req.body);
+    
+      user
+        .save()
+        .then((data) => {
+          res.json({ message: 'New user added!' });
+        })
+        .catch((err) => console.log(err));
+})
+
+app.put("/api/users/:id", (req, res)=>{
+    if (!req.body.name || !req.body.age || !req.body.password || !req.body.email) {
+        res.status(400).json({ message: 'All fields are required' });
+        return;
+      }
+      // -- if valdiation passes, updating car
+      let userId = req.params.id;
+    
+      User.findByIdAndUpdate(userId, req.body)
+        .then((data) => res.json({ message: 'User updated!' }))
+        .catch((err) => console.log(err));
+})
+
+app.delete("/api/users/:id", (req, res)=>{
+    let userId = req.params.id;
+
+    User.findByIdAndDelete(userId)
+      .then((data) => res.json({ message: 'User deleted!' }))
+      .catch((err) => console.log(err));
+})
+
+
