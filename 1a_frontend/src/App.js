d5ed7c1c7943952d838d0f8293db641ab5eab577
vytautas-karvelis <vytkarvelis@gmail.com>
2021-09-17 11:19:05 +0200

    delete section finished

--- 1a_frontend/src/App.js
+++ 1a_frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import ViewUsers from './components/ViewUsers'
 import PostUser from './components/PostUser';
 import UpdateUser from './components/UpdateUser'
+import DeleteUser from './components/DeleteUser';
 function App() {
   return (
     <div className="App">
         <ViewUsers/>
         <PostUser />
         <UpdateUser />
+        <DeleteUser/>
     </div>
   );
 }
--- 1a_frontend/src/components/DeleteUser.js
+++ 1a_frontend/src/components/DeleteUser.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 const DeleteUser = () => {
+
+    const [userId, setUserId] = useState('')
+
+    const deleteUser = (e) => {
+        e.preventDefault()        
+
+        axios.delete('http://localhost:5000/api/users/'+ userId)
+            .then(response=>{               
+                console.log(response)               
+               
+            })
+            .catch(err=>{
+                console.log(err)     
+            })
+        }
+
+
     return (
         <div>
-                <form id="logInForm" onSubmit={postUser}>
-                <div className="form-control">
-                <label className="form-label" >Id</label>
-                <input                            
-                    type="text"
-                    value={userId}
-                    onChange={(e)=>setUserId(e.target.value)}
-                    required                           
-                />
-                </div>
+            <h2>Delete User</h2>
+                <form id="logInForm" onSubmit={deleteUser}>
+                    <div className="form-control">
+                    <label className="form-label" >Id</label>
+                    <input                            
+                        type="text"
+                        value={userId}
+                        onChange={(e)=>setUserId(e.target.value)}
+                        required                           
+                    />
+                    </div>
+
+                    <div className="form-control">
+                    <input
+                        type="submit"
+                        value="Delete specified User"
+                        className="btn-primary btn-primary-submit"
+                    />
+                    </div>
+                </form>
         </div>
     )
 }
